d4aff477182655665f05ca738eb33d2f82de67a8
Chandan Gupta <chandankumarguptabgt@gmail.com>
2023-08-07 06:23:29 +0530

    Dictionary Feature Completed

--- lib/screens/dictionary.dart
+++ lib/screens/dictionary.dart
@@ -1,10 +1,196 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class Dictionary extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+class Dictionary extends StatefulWidget {
   Dictionary({super.key});
 
   @override
+  State<Dictionary> createState() => _DictionaryState();
+}
+
+class _DictionaryState extends State<Dictionary> {
+
+  var visibility = false;
+  var wordController = TextEditingController();
+  var definition = '';
+
+  void getDefinition() async {
+    EasyLoading.show();
+    definition = '';
+    var headers = {
+      'X-Api-Key': 'C0sJdYc5Tc5PYYxHJpNslw==2Is9I4yHKmNFDKKR'
+    };
+    var request = http.Request('GET', Uri.parse('https://api.api-ninjas.com/v1/dictionary?word=${wordController.text.toLowerCase()}'));
+
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      var dataJson = await response.stream.bytesToString();
+      var data = jsonDecode(dataJson);
+      if(data['valid']){
+        definition = data["definition"];
+        visibility = true;
+        setState(() {
+
+        });
+      }
+      else{
+        EasyLoading.showToast(
+          'Word Not Defined!',
+          toastPosition: EasyLoadingToastPosition.center,
+          duration: Duration(seconds: 1),
+        );
+      }
+      EasyLoading.dismiss();
+    }
+    else {
+      EasyLoading.dismiss();
+      EasyLoading.showToast(
+        response.reasonPhrase.toString(),
+        toastPosition: EasyLoadingToastPosition.center,
+        duration: Duration(seconds: 1),
+      );
+    }
+
+  }
+
+  void copyText() {
+    Clipboard.setData(ClipboardData(text: definition));
+    EasyLoading.showToast(
+      'Text Copied !',
+      toastPosition: EasyLoadingToastPosition.bottom,
+      duration: Duration(seconds: 1),
+    );
+  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.05,
+            vertical: MediaQuery.of(context).size.height * 0.01),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: [Colors.white, Theme.of(context).primaryColor],
+              center: Alignment.center,
+              radius: 1,
+              tileMode: TileMode.mirror,
+            ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+              ),
+              TextField(
+                controller: wordController,
+                keyboardType: TextInputType.name,
+                cursorColor: Theme.of(context).primaryColor,
+                decoration: InputDecoration(
+                    label: Text('Enter Your Word'),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(
+                          color: Theme.of(context).primaryColor,
+                          width: 1,
+                        )),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(
+                          color: Colors.grey,
+                          width: 1,
+                        )),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    )),
+                style: TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+                child: ElevatedButton(
+                  onPressed: getDefinition,
+                  child: Text(
+                    'Get  Definition',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.025,
+              ),
+              Visibility(
+                  visible: visibility,
+                  child: Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: Theme.of(context).primaryColor,
+                        width: 0.5,
+                      ),
+                    ),
+                    child: Text(
+                      definition,
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  )
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height*0.025,
+              ),
+              Visibility(
+                visible: visibility,
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.6,
+                  child: ElevatedButton(
+                    onPressed: copyText,
+                    child: Text(
+                      'Copy Definition',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        )
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
